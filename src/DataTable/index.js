5b7c35bc9e9d9f7d6591e41aa4569de470d6daff
momo <momo@mail.go.kr>
2023-12-11 21:44:08 +0900

    edit

--- src/DataTable/index.js
+++ src/DataTable/index.js
@@ -1,16 +1,22 @@
 import react , {useState , useEffect} from "react";
 import axios from "axios";
-import {Table} from "antd";
+//datatable ,
+import {Table, Popconfirm, Button , Space , Form} from "antd";
+//널체크
+import {isEmpty} from "lodash";
 
 const DataTable = () => {
 
     const [gridData,setGridData] = useState([]);
     const [loading,setLoading] = useState(false);
+    const [editingKey, setEditingKey] = useState("");
+    const [editRow, setEditRow] = useState(false);
 
     useEffect(()=>{
         loadData();
     }, [])
 
+    //데이터 로딩
     const loadData =async () =>{
         setLoading(true)
         const response = await axios.get("https://jsonplaceholder.typicode.com/comments")
@@ -19,16 +25,34 @@ const DataTable = () => {
     }
 
     // console.log("gridData",gridData);
+    //삭제
+    const handleDelete = (value) => {
+        const dataSource = [...modifyData];
+        const filterData = dataSource.filter( item => item.id !== value.id);
+        setGridData(filterData);
+    }
 
+    const save = () =>{
+
+    }
+
+    const edit = () =>{
+        
+    }
+
+    //원본 데이터를 key , comment 추가
     const modifyData = gridData.map( ({body, ...item}) => (
         {
         ...item,
         key:item.id,
-        comment:body
+        // comment: body
+        //body 값이 없는 경우 재 정의.. 왜이렇게 하는걸까..
+        comment: isEmpty(body) ? item.comment : body
         }
     ))
     // console.log('modifyData ' , modifyData)
 
+    //컬럼
     const columns = [
         {
             title:"ID",
@@ -55,12 +79,68 @@ const DataTable = () => {
         {
             title:"Actions",
             dataIndex:"actions",
-            align:"center",            
+            align:"center",   
+            render: (_,record) => {
+                //custom button
+                return modifyData.length >=1 ? 
+                (
+                    <Space>
+                        <Popconfirm
+                        title="Sure to delete?"
+                        onConfirm={() =>handleDelete(record)}>
+                            <Button type="primary" danger>
+                                Delete
+                            </Button>
+                        </Popconfirm>
+                                           
+                        {editRow ?  (
+                                <span>
+                                    <Button onClick={(e) => console.log(e)}
+                                        type="primary"
+                                        style={{maringRight:8}}
+                                    >Save
+                                    </Button>
+                                    <Popconfirm
+                                        title="Sure to Cancle?"
+                                        onConfirm={()=> setEditRow(false)}
+                                    ><Button>Cancle</Button>
+                                    </Popconfirm>
+                                </span>
+                            ) : (
+                                <Button onClick={ () => setEditRow(true)}
+                                    type="primary"
+                                >Edit
+                                </Button>
+                            )
+                        }
+                    </Space>
+                ): null
+            }
         }
     ]
+
+    const isEditing = (record) =>{
+        return record.key === editingKey;
+    }
+
+    const mergedColumns = columns.map( (col) =>{
+        if (!col.editable){
+            return col
+        }
+        return{
+            ...col,
+            onCell : (record) => ({
+                record,
+                dataIndex: col.dataIndex,
+                title : col.title,
+                editing : isEditing(record),
+            })
+        }
+    });
+
     return (
         <div>
-            <Table columns={columns}
+            <Table columns={mergedColumns}
             dataSource={modifyData}
             bordered
             loading={loading}
